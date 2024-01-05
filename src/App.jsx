191c0a196a4101f992ee45cc378f914993b0a5d1
arunkumar0512 <140463247+arunkumar0512@users.noreply.github.com>
2024-01-05 18:09:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './Home';
 import NavBar from './NavBar';
 import {Route,Routes} from 'react-router-dom'
 import About from './About';
-import Blog from './Blog';
+
 import Inventory from './Inventory';
 import Manageitem from './Manageitem';
 import Additem from './Additem';
